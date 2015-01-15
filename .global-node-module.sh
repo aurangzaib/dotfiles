582cdd18b67eda3e04ea8c3403243e95f4f79b93
aurangzaib <aurangzaibsiddiqui@gmail.com>
2015-01-16 02:06:32 +0500

    someother useful modules

--- .global-node-module.sh
+++ .global-node-module.sh
@@ -4,4 +4,7 @@ npm install -g grunt-cli &&
 npm install -g gulp-cli &&
 npm install -g ionic && 
 npm install -g cordova &&
+npm install -g coffee-script &&
+npm install -g js2coffee &&
+npm install -g less &&
 npm install -g ios-sim
